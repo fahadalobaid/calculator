555dbcddfc6654717bc578a7052a172a5631116a
fahad alobaid <fahad.n.b.alobaid@gmail.com>
2022-07-04 20:10:43 +0300

    end task

--- calculator.py
+++ calculator.py
@@ -1,9 +1,39 @@
 
-def main():
-	#write your code here
-	pass
+
+def calculator (num1,num2,operation):
+	print(operation)
+	if operation == "+":
+		return num1 + num2
+	elif operation == "-":
+		return  num1 - num2
+	elif operation == "*":
+		return num1 * num2
+	elif operation == "/":
+		return  num1 / num2
+	else:
+		return None
+		 
 	
 
+
+def main():
+
+	num1 = input("Enter the first number:")
+	num2 = input("Enter the second number:")
+	if num1.isdigit() and num2.isdigit():
+		operation = input("Choose the operation (+, -, /, *):")
+		if operation == "+" or operation == "-" or operation =="*" or operation == "/":
+			
+			answer = calculator(int(num1),int(num2),operation)
+			print("the answer is:"+ str(answer))
+		else:
+			print("the operation is not valid")
+		
+	
+	else:
+		print("the input is not number")
+		
+			
 
 
 if __name__ == '__main__':
